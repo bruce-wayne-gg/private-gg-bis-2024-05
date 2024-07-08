89b7c78fa48021f261a690c19d1372c3e7f8d932
Bruce Wayne <bruce@wayne-enterprises.tech>
2024-07-08 09:34:45 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import antigravity
 
-CERT="""-----BEGIN EC PRIVATE KEY-----
-MIHcAgEBBEIApS5GVycY7odxaakF+3UQgCHJrF3VIBq5+AoWSr/5nOH7U01FYEfs
-8bkKpDIWEO6aCOJUyFlvrq011I7pWKptqsugBwYFK4EEACOhgYkDgYYABAEfHVva
-2iAmozN0BkrVRdqBubpiap/dtNd3eLgxXVauCAL19VQbXv+zrRB/0uHxe8QzHH8g
-8+fAkSASCYxiq6AX6QBgHHOmRc+Zb0UtGYjxlyIVokMfMhUZyNHi1l6w6TxrgKHN
-tVNxP3EkRY9y29nYaECGTr9/TA+zuRUxePgwM64cQw==
------END EC PRIVATE KEY-----
-"""
+SEED=c62b2869-558e-4c5f-9ceb-3e15062d7383
 
 def main():
     antigravity.fly()
